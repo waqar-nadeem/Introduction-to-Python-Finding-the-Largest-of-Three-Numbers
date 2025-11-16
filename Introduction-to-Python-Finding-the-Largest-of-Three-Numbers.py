4f534f68b3bff4bf0bcce4de4e3bdc8172ce2283
waqar-nadeem <waqarnadeem088@gmail.com>
2025-11-16 12:25:50 +0500

    Create Introduction-to-Python-Finding-the-Largest-of-Three-Numbers.py

---
+++ Introduction-to-Python-Finding-the-Largest-of-Three-Numbers.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+if num1 > num2:
+    if num1 > num3:
+        print("The largest number is:", num1)
+    else:
+        print("The largest number is:", num3)
+else:
+    if num2 > num3:
+        print("The largest number is:", num2)
+    else:
+        print("The largest number is:", num3)
